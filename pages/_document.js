bba2af56d00b10549fa16e1c890ce96bb4970013
Seth Anderson <sdande02@gmail.com>
2021-02-25 13:45:56 -0700

    configure mui to prevent fouc

--- pages/_document.js
+++ pages/_document.js
@@ -1,33 +1,68 @@
-import Document from 'next/document'
-import { ServerStyleSheet } from 'styled-components'
-
-export default class MyDocument extends Document {
+import React from 'react'
+import NextDocument from 'next/document'
+import { ServerStyleSheet as StyledComponentSheets } from 'styled-components'
+import { ServerStyleSheets as MaterialUiServerStyleSheets } from '@material-ui/core/styles'
+export default class Document extends NextDocument {
     static async getInitialProps(ctx) {
-        const sheet = new ServerStyleSheet()
+        const styledComponentSheet = new StyledComponentSheets()
+        const materialUiSheets = new MaterialUiServerStyleSheets()
         const originalRenderPage = ctx.renderPage
-
         try {
             ctx.renderPage = () =>
                 originalRenderPage({
-                    enhanceApp: (App) => (props) =>
-                        sheet.collectStyles(<App {...props} />),
+                    enhanceApp: App => props =>
+                        styledComponentSheet.collectStyles(
+                            materialUiSheets.collect(<App {...props} />),
+                        ),
                 })
-
-            const initialProps = await Document.getInitialProps(ctx)
+            const initialProps = await NextDocument.getInitialProps(ctx)
             return {
                 ...initialProps,
-                styles: (
-                    <>
+                styles: [
+                    <React.Fragment key="styles">
                         {initialProps.styles}
-                        {sheet.getStyleElement()}
-                    </>
-                ),
+                        {materialUiSheets.getStyleElement()}
+                        {styledComponentSheet.getStyleElement()}
+                    </React.Fragment>,
+                ],
             }
         } finally {
-            sheet.seal()
-        } 
+            styledComponentSheet.seal()
+        }
     }
 }
+// above is code from https://js.plainenglish.io/ssr-with-next-js-styled-components-and-material-ui-b1e88ac11dfa
+
+// import Document from 'next/document'
+// import { ServerStyleSheet } from 'styled-components'
+
+// export default class MyDocument extends Document {
+//     static async getInitialProps(ctx) {
+//         const sheet = new ServerStyleSheet()
+//         const originalRenderPage = ctx.renderPage
+
+//         try {
+//             ctx.renderPage = () =>
+//                 originalRenderPage({
+//                     enhanceApp: (App) => (props) =>
+//                         sheet.collectStyles(<App {...props} />),
+//                 })
+
+//             const initialProps = await Document.getInitialProps(ctx)
+//             return {
+//                 ...initialProps,
+//                 styles: (
+//                     <>
+//                         {initialProps.styles}
+//                         {sheet.getStyleElement()}
+//                     </>
+//                 ),
+//             }
+//         } finally {
+//             sheet.seal()
+//         } 
+//     }
+// }
 
 // TODO: below was to get fonts. above is so styled components work. want both.
 
