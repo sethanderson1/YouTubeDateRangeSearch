94d4c662391ad38e9d0b486dfa73445c78fa40be
Seth Anderson <sdande02@gmail.com>
2021-02-27 13:35:45 -0700

    fetches data properly

--- components/Form.js
+++ components/Form.js
@@ -16,8 +16,7 @@ import SearchIcon from '@material-ui/icons/Search';
 import DatePicker from './DatePicker'
 import styled from 'styled-components'
 import { FormContext } from '../context/FormContext';
-
-
+import axios from 'axios';
 
 const OuterWrap = styled.div`
 margin: 0 auto;
@@ -87,50 +86,83 @@ const DatesWrap = styled.div`
 
 export const Form = () => {
     console.log('%cForm renders', 'color:green')
-    const [sortOption, setSortOption] = useState("relavance");
+    const [sortOption, setSortOption] = useState("relevance");
     const [query, setQuery] = useState("");
     const context = useContext(FormContext);
-    const { start, setStart, end, setEnd, date, setDate } = context;
+    const { start, setStart, end, setEnd, date, setDate, res, setRes } = context;
+    const apiKey = process.env.NEXT_PUBLIC_API_KEY;
+    const maxResults = 10;
+    let data;
 
-    useEffect(() => {
-
-    }, [])
-
-    const handleSubmit = (e) => {
+    const handleSubmit = async (e) => {
         e.preventDefault();
         console.log('query', query)
         console.log('sortOption', sortOption)
         console.log('start', start)
         console.log('end', end)
-
-        // fetchData(query, sortOptions, start)
+        data = await fetchData();
+        console.log('data', data)
     }
 
-    const fetchData = (query, sortOptions, start) => {
+    useEffect(() => {
+        setRes(data);
+    }, [data])
 
+
+    const params = {
+        key: apiKey,
+        q: query,
+        part: 'snippet',
+        maxResults,
+        type: 'video',
+        order: sortOption,
+        publishedAfter: `${start}T05:55:00Z`,
+        publishedBefore: `${end}T05:55:00Z`
+    };
+
+    const searchURL = `https://youtube.googleapis.com/youtube/v3/search`;
+    const queryString = formatQueryParams(params);
+    const url = searchURL + '?' + queryString;
+    function formatQueryParams(params) {
+        const queryItems = Object.keys(params)
+            .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`);
+        return queryItems.join('&');
+    }
+
+
+    const fetchData = async () => {
+        console.log('url in fetchData', url)
+        try {
+            const res = await axios(url);
+            console.log('res', res)
+            setRes(res.data)
+        } catch (e) {
+            console.log('e', e)
+
+        }
     }
 
     return (
         <OuterWrap>
             <FormWrap onSubmit={(e) => handleSubmit(e)} >
                 {/* <FormControl component="fieldset" > */}
-                    <SearchWrap>
-                        <TextField variant="outlined" fullWidth={true} value={query} onChange={(e) => setQuery(e.target.value)} />
-                        <Button variant='outlined' type="submit"><SearchIcon /></Button>
-                    </SearchWrap>
-                    <OptionsWrap >
-                        <SortOptionWrap>
-                            <RadioGroup aria-label="video sort options" name="sort-options" value={sortOption} onChange={(e) => setSortOption(e.target.value)}>
-                                <FormControlLabel value="relavance" control={<Radio />} label="Relavance" />
-                                <FormControlLabel value="uploadDate" control={<Radio />} label="Upload Date" />
-                                <FormControlLabel value="viewCount" control={<Radio />} label="View Count" />
-                            </RadioGroup>
-                        </SortOptionWrap>
-                        <DatesWrap>
-                            <DatePicker label="start" />
-                            <DatePicker label="end" />
-                        </DatesWrap>
-                    </OptionsWrap>
+                <SearchWrap>
+                    <TextField variant="outlined" fullWidth={true} value={query} onChange={(e) => setQuery(e.target.value)} />
+                    <Button variant='outlined' type="submit"><SearchIcon /></Button>
+                </SearchWrap>
+                <OptionsWrap >
+                    <SortOptionWrap>
+                        <RadioGroup aria-label="video sort options" name="sort-options" value={sortOption} onChange={(e) => setSortOption(e.target.value)}>
+                            <FormControlLabel value="relevance" control={<Radio />} label="Relevance" />
+                            <FormControlLabel value="date" control={<Radio />} label="Upload Date" />
+                            <FormControlLabel value="viewCount" control={<Radio />} label="View Count" />
+                        </RadioGroup>
+                    </SortOptionWrap>
+                    <DatesWrap>
+                        <DatePicker label="start" />
+                        <DatePicker label="end" />
+                    </DatesWrap>
+                </OptionsWrap>
                 {/* </FormControl> */}
             </FormWrap>
         </OuterWrap >
