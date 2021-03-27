87c5d42e4e107a8c92d05687fe88724e42bcce4c
Seth Anderson <sdande02@gmail.com>
2021-03-26 18:14:16 -0700

    wip pagination

--- components/PageNav.js
+++ components/PageNav.js
@@ -6,6 +6,7 @@ import {
 import styled from 'styled-components'
 import { FormContext } from '../context/FormContext';
 import fetchData from '../utils/fetchData';
+// import { nanoid } from 'nanoid'
 
 const NavWrap = styled.div`
     display:flex;
@@ -46,40 +47,49 @@ export const PageNav = ({ executeScroll }) => {
         lastPage,
         setLastPage
     } = context;
-    console.log('pageTokens', pageTokens)
+    // console.log('pageTokens', pageTokens)
     console.log('curPage', curPage)
 
-    const setTokens = (prevPageToken, nextPageToken) => {
-        if (prevPageToken && pageTokens[0] === 'DUMMY') {
-            // console.log('setTokens opt 1')
-            // setPageTokens(formerTokens => [prevPageToken, ...formerTokens])
-            const newTokenArr = [prevPageToken, pageTokens[1], nextPageToken];
-            // console.log('newTokenArr', newTokenArr)
-            setPageTokens(newTokenArr);
-        } else {
-            // console.log('setTokens opt 2')
-            // console.log('pageTokens', pageTokens)
-            setPageTokens(formerTokens => [...formerTokens, nextPageToken]);
+    const handleSetTokens = (prevPageToken, nextPageToken) => {
+        console.log('handleSetTokens ran')
+        console.log('prevPageToken in handleSetTokens', prevPageToken)
+        console.log('nextPageToken in handleSetTokens', nextPageToken)
+        // if we're at the first fetch, we should end up with
+        // ['DUMMY', SOMETOKEN]
+        if (pageTokens[0] === 'DUMMY' && pageTokens.length === 1) {
+            setPageTokens([prevPageToken, nextPageToken])
         }
+        // if (prevPageToken && pageTokens[0] === 'DUMMY') {
+        //     // console.log('handleSetTokens opt 1')
+        //     // setPageTokens(formerTokens => [prevPageToken, ...formerTokens])
+        //     const newTokenArr = [prevPageToken, pageTokens[1], nextPageToken];
+        //     // console.log('newTokenArr', newTokenArr)
+        //     setPageTokens(newTokenArr);
+        // } else {
+        //     // console.log('handleSetTokens opt 2')
+        //     // console.log('pageTokens', pageTokens)
+        //     setPageTokens(formerTokens => [...formerTokens, nextPageToken]);
+        // }
     }
 
     const handleClickNext = async () => {
+        console.log('%c handleClickNext ran', 'color:orange')
+
+        console.log('curPage', curPage)
         const nextPageToken = pageTokens[curPage];
         const res = await fetchData({ query, maxResults, sortOption, start, end, pageToken: nextPageToken })
-        // ====
         let secondNextPageToken = res.nextPageToken
         console.log('secondNextPageToken', secondNextPageToken)
         const resDataSecondFetch = await fetchData({ query: query, maxResults, sortOption, start, end, pageToken: secondNextPageToken });
-        // if no next page token, handle accordingly in pagenav
+        console.log('resDataSecondFetch', resDataSecondFetch)
         if (!resDataSecondFetch.nextPageToken) {
             setNextUndef(true)
-            setLastPage(curPage)
+            setCurPage(prevPage => prevPage + 1)
+            setLastPage(curPage + 1)
         }
-        console.log('resDataSecondFetch', resDataSecondFetch)
 
-        // ====
-        setRes(res)
         setCurPage(prevPage => prevPage + 1)
+        setRes(res)
         executeScroll();
     }
 
@@ -93,10 +103,15 @@ export const PageNav = ({ executeScroll }) => {
     }
 
     const handleClickPageNum = async (token, i) => {
-        const res = await fetchData({ query, maxResults, sortOption, start, end, pageToken: token });
-        setRes(res);
-        setCurPage(i + 1);
-        executeScroll();
+        console.log('i in pagenum', i)
+        if (pageTokens.length === i + 1) {
+            handleClickNext()
+        } else {
+            const res = await fetchData({ query, maxResults, sortOption, start, end, pageToken: token });
+            setRes(res);
+            setCurPage(i + 1);
+            executeScroll();
+        }
     }
 
     const isCurrentPage = (token, i) => {
@@ -107,42 +122,29 @@ export const PageNav = ({ executeScroll }) => {
 
 
     useEffect(() => {
+        console.log('useEffect in PageNav ran')
+        console.log('res in useeffect', res)
+
+        // if (res) {
         const nextPageToken = res?.nextPageToken;
         console.log('nextPageToken', nextPageToken)
         const prevPageToken = res?.prevPageToken;
         console.log('prevPageToken', prevPageToken)
         setNextUndef(false);
 
-        // if (res?.lastPage) {
-        //     console.log('this is the last page')
-        // }
-
-        console.log('curPage', curPage)
-        console.log('lastPage', lastPage)
+        console.log('curPage in useeffect', curPage)
+        console.log('lastPage in useeffect', lastPage)
         if (lastPage === curPage) {
-            // next is grayed out
             setNextUndef(true)
         } else {
-            console.log('lastPage not current page')
-            setTokens(prevPageToken, nextPageToken);
+            if (!tokenAlreadyExists(nextPageToken)) {
+                console.log('about to set tokens')
+                handleSetTokens(prevPageToken, nextPageToken)
+            }
         }
-
-
-
-        // if ((prevPageToken || nextPageToken)) {
-        //     if (!(tokenAlreadyExists(prevPageToken) || tokenAlreadyExists(nextPageToken))) {
-        //         console.log('setting tokens')
-        //         if (nextPageToken !== undefined) {
-        //             if (lastPage === curPage) {
-
-        //             } else {
-        //                 setTokens(prevPageToken, nextPageToken);
-        //             }
-        //         } else {
-        //             setNextUndef(true);
-        //         }
-        //     }
         // }
+
+
     }, [res])
 
     const tokenAlreadyExists = (token) => {
@@ -150,12 +152,21 @@ export const PageNav = ({ executeScroll }) => {
     }
 
     const renderNav = () => {
+        console.log('hasSearched', hasSearched)
         if (hasSearched) {
             return (
                 <NavWrap>
                     <Button onClick={() => handleClickPrev()} disabled={curPage === 1 ? true : false} >Prev</Button>
-                    {pageTokens.map((token, i) => <Button key={i} onClick={() => handleClickPageNum(token, i)} disabled={isCurrentPage(token, i)}>{i + 1}</Button>)}
-                    <Button onClick={() => handleClickNext()} disabled={nextUndef ? true : false}>Next</Button>
+                    {pageTokens.map((token, i) => {
+                        console.log('pageTokens in button', pageTokens)
+                        // console.log('i in button', i)
+                        // console.log('token', token)
+                        return <Button key={i} onClick={() => handleClickPageNum(token, i)} disabled={isCurrentPage(token, i)}>{i + 1}</Button>
+                    }
+
+                    )}
+                    {/* <Button onClick={() => handleClickNext()} disabled={nextUndef ? true : false}>Next</Button> */}
+                    <Button onClick={() => handleClickNext()} disabled={curPage === lastPage ? true : false}>Next</Button>
                 </NavWrap>
             )
         } else {
