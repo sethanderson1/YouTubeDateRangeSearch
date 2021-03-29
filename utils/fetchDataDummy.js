37fe3d4d6ad19b6cf970be6ee561e98111cfe9b4
Seth Anderson <sdande02@gmail.com>
2021-03-28 21:32:41 -0700

    merge

--- utils/fetchDataDummy.js
+++ utils/fetchDataDummy.js
@@ -9,8 +9,8 @@ const imageUris = [
     'https://via.placeholder.com/600/24f355',
     'https://via.placeholder.com/600/d32776',
     'https://via.placeholder.com/600/f66b97',
-    'https://via.placeholder.com/600/b0f7cc',
-    'https://via.placeholder.com/600/54176f',
+    // 'https://via.placeholder.com/600/b0f7cc',
+    // 'https://via.placeholder.com/600/54176f',
     // 'https://via.placeholder.com/600/51aa97',
     // 'https://via.placeholder.com/600/810b14',
     // 'https://via.placeholder.com/600/1ee8a4',
@@ -58,18 +58,16 @@ export default async function fetchDataDummy({ query, sortOption, start, end, ma
         console.log('dummyPageTokens', dummyPageTokens)
         console.log('pageToken in setPageTokens', pageToken)
         console.log('index in setPageTokens', index)
-        // TODO: index needs to refer to the index of the items, NOT the dummyTokens
-        // change so that it references correct index of items
 
         const startInd = index * maxResults
         console.log('startInd', startInd)
         const endInd = startInd + maxResults
         console.log('endInd', endInd)
-        
+
         if (index === 0) {
             res.prevPageToken = undefined
             res.nextPageToken = dummyPageTokens[index + 1]
-        } else if ( index === dummyPageTokens.length - 1) {
+        } else if (index === dummyPageTokens.length - 1) {
             res.prevPageToken = dummyPageTokens[index - 1]
             res.nextPageToken = undefined
         } else {
@@ -80,34 +78,12 @@ export default async function fetchDataDummy({ query, sortOption, start, end, ma
 
 
 
- 
-        // if (index  === dummyPageTokens.length - 1) {
-        // // if (index + 1 === dummyPageTokens.length - 1) {
-        // // if (index + 1 === dummyPageTokens.length - 1) {
-        //     console.log('%c went into first block since this is last page', 'font-size:20px')
-        //     res.items = items.slice(index + 1)
-        //     console.log('res.items', res.items)
 
-        //     res.prevPageToken = dummyPageTokens[index - 1]
-        //     res.nextPageToken = undefined
-        // } else if (index === 0) {
-        //     console.log('%c i dont think youll ever see me', 'font-size:30px')
-        //     res.items = items.slice(0, maxResults)
-
-        //     res.prevPageToken = undefined
-        //     res.nextPageToken = dummyPageTokens[index + 1]
-
-        // } else {
-        //     res.items = items.slice(index + 1, index + 1 + maxResults)
-
-        //     res.prevPageToken = dummyPageTokens[index - 1]
-        //     res.nextPageToken = dummyPageTokens[index + 1]
-        // }
     }
 
     if (query === '') {
-        // dummyPageTokens = ['A', 'B', 'C']
-        dummyPageTokens = ['A', 'B', 'C', 'D']
+        dummyPageTokens = ['A', 'B', 'C']
+        // dummyPageTokens = ['A', 'B', 'C', 'D']
         // dummyPageTokens = ['A', 'B', 'C']
         if (!pageToken) {
             console.log('went to !pageToken block')
@@ -119,7 +95,7 @@ export default async function fetchDataDummy({ query, sortOption, start, end, ma
             console.log('went to has pagetoken block with token: ', pageToken)
             setPageTokens(dummyPageTokens)
         }
-        console.log('%c res.items','font-size:20px', res.items)
+        console.log('%c res.items', 'font-size:20px', res.items)
 
     }
 
